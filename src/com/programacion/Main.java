685b753fb1278ce9c2a3ebfefef254b861e488d8
AlejandroQuintaFarina <aquintafarina@danielcastelao.org>
2022-03-07 10:22:16 +0100

    Metodo fecha creado y el metodo de imprimir ticket creado y funcionando los dos

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -1,8 +1,14 @@
 package com.programacion;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	//
+
+      //  ArrayList Regist = new ArrayList();
+	Metodos met = new Metodos();
+    met.Fecha();
+    //met.abrirarchivo();
     }
 }
--- src/com/programacion/Metodos.java
+++ src/com/programacion/Metodos.java
@@ -1,4 +1,30 @@
 package com.programacion;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Metodos {
+
+    public void Fecha (){
+        Date fecha = new Date();
+        System.out.println(new SimpleDateFormat("dd-MM-yyyy").format(fecha));
+    }
+
+    /* public void abrirarchivo(String archivo){
+
+        try {
+
+            File objetofile = new File (archivo);
+            Desktop.getDesktop().open(objetofile);
+
+        }catch (IOException ex) {
+
+            System.out.println(ex);
+
+        }
+
+    }*/
 }
